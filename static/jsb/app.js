ccd7fbc96c7cec8d455e58189dc452e290534cd9
Michael Len <me@michaellen.com>
2017-04-01 21:52:28 -0700

    Add emotes instead of grouping by channel

--- static/jsb/app.js
+++ static/jsb/app.js
@@ -1,5 +1,28 @@
+
+var Emote = React.createClass({
+  propTypes: {
+    template: React.PropTypes.object.isRequired,
+    emote: React.PropTypes.object.isRequired,
+  },
+  getInitialState: function() {
+    return null;
+  },
+
+
+  componentDidMount: function() {
+  }, 
+  
+  render: function() {
+    return (
+        <div className="emote">
+          <img src={this.props.template.small.replace("{image_id}", this.props.emote.image_id)} width="10px" height="10px"/>
+        </div>
+      );
+  }
+});
 var Channel = React.createClass({
   propTypes: {
+    template: React.PropTypes.object.isRequired,
     name: React.PropTypes.string.isRequired,
     channel: React.PropTypes.object.isRequired,
   },
@@ -14,8 +37,13 @@ var Channel = React.createClass({
   render: function() {
     return (
         <div className="channel">
-          <div>{this.props.channel.title}</div>
-          <div>{this.props.channel.channel_id}</div>
+          
+
+          { Object.keys(this.props.channel.emotes).map(function (key) {
+          return (
+            <Emote key={key} emote={this.props.channel.emotes[key]} template={this.props.template}/>
+            );
+        }, this)}
         </div>
       );
   }
@@ -25,7 +53,7 @@ var AppPage = React.createClass({
   propTypes: {
   },
   getInitialState: function() {
-    return {channels:{}, template: {}};
+    return {channels:{}, template: {}, farthest: 100};
   },
 
 
@@ -34,18 +62,35 @@ var AppPage = React.createClass({
     $.getJSON('/vendor/subscriber.json', function(data) {
       this.setState({"template": data.template, "channels": data.channels});
     }.bind(this));
+      setTimeout(this.incrementFarthest, 500);
   }, 
   
+  incrementFarthest: function() {
+    if (this.state.farthest < 2000) {
+      this.setState({"farthest":this.state.farthest + 100});
+      setTimeout(this.incrementFarthest, 1000);
+    } else {
+      console.log ("iterated to farthest" + this.state.farthest);
+    }
+  },
   render: function() {
+    var count = this.state.farthest;
     return (
         <div>
           <h1 ref="splash" className="splash">
           </h1>
           <div id="channel-container">
-          { Object.keys(this.state.channels).map(function (key) {
-          return (
-            <Channel key={key} name={key} channel={this.state.channels[key]}/>
-            );
+          { Object.keys(this.state.channels).map(function (key, index, arr) {
+            if (index > this.state.farthest) return;
+            return Object.keys(this.state.channels[key].emotes).map(function (emotekey) {
+            return (
+              <Emote key={emotekey + ", " + key} emote={this.state.channels[key].emotes[emotekey]} template={this.state.template}/>
+              );
+
+            }, this);
+            //return (
+            //  <Channel key={key} name={key} channel={this.state.channels[key]} template={this.state.template}/>
+            //  );
         }, this)}
           </div>
         </div>
